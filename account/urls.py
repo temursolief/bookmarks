ca845e1d07d51aeee427964c246c888dfd957373
Timur Soliev <temursoliev2110@gmail.com>
2022-10-02 18:27:15 +0500

    feat: added login.html in a new dir "templates" in account and updated account.urls with native auth

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import user_login
 
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
