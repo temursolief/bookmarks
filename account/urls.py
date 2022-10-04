25da36327837bfc89f2ab17cb22debfdc7136c67
Timur Soliev <temursoliev2110@gmail.com>
2022-10-04 09:42:37 +0500

    added dashboard view's url to urlpatterns'

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import user_login
+from .views import dashboard, user_login
 
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
