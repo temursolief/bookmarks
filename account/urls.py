5250032c55ed23a5a98d294e158cd7a012265e80
Timur Soliev <temursoliev2110@gmail.com>
2022-10-04 09:55:24 +0500

    added password change view urls to the urlpatterns

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('password-change/', 
+            auth_views.PasswordChangeView.as_view(),
+            name='password_change'),
+    path('password-change/done/',
+            auth_views.PasswordChangeDoneView.as_view(),
+            name='password_change_done'),
     path('', dashboard, name='dashboard'),
 ]
