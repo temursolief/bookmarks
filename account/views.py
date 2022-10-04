2572e52fc71dfdfbb867b9763e857597c752feae
Timur Soliev <temursoliev2110@gmail.com>
2022-10-04 19:12:00 +0500

    added edit method for Profile and User

--- account/views.py
+++ account/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import (LoginForm, UserRegistrationForm, 
+            UserEditForm, ProfileEditForm)
+from .models import Profile
 
 
 def register(request):
@@ -15,6 +17,7 @@ def register(request):
                 user_form.cleaned_data['password']
             )
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 
                 'account/register_done.html',
                 {'new_user': new_user})
@@ -23,6 +26,24 @@ def register(request):
     return render(request, 
             'account/register.html',
             {'user_form': user_form,})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, 
+                data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', 
+            {'user_form': user_form,
+            'profile_form': profile_form,})
 
 
 
