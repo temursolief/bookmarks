83ab71625cdb03e80954b59804ac9f138b70fb4b
Timur Soliev <temursoliev2110@gmail.com>
2022-10-05 19:37:42 +0500

    changed the dirs of register and register_done templates

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', '127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party
+    'social_django',
 ]
 
 MIDDLEWARE = [
