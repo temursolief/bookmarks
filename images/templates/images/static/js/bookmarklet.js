320308f137e40c6ba62438ca04e3f0212a657a3e
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 08:12:34 +0500

    created bookmarkletLaunch function

--- images/templates/images/static/js/bookmarklet.js
+++ images/templates/images/static/js/bookmarklet.js
@@ -23,3 +23,18 @@ boxHtml = `
         <div class="images"></div>
     </div>`;
 body.innerHTML += boxHtml;
+
+
+function bookmarkletLaunch(){
+    bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+
+    imagesFound.innerHTML = '';
+    bookmarklet.style.display = 'block';
+    bookmarklet.querySelector("#close")
+        .addEventListener('click', function(){
+            bookmarklet.style.display = 'none';
+        });
+}
+
+bookmarkletLaunch();
