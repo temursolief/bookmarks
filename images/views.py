eb3f9e3e9eb409d1eef8e0f3259a5ca0c4d1adfa
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 14:12:21 +0500

    added image_list

--- images/views.py
+++ images/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
@@ -49,3 +50,24 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse('status': 'error')
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+            'images/image/list.html',
+            {"section":"images",
+            "images": images,})
