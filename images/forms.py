22039d46abf1f6a5128ac31500509f27e4a87b7f
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 07:08:44 +0500

    added url_checker for png, jpg and jpeg extensions

--- images/forms.py
+++ images/forms.py
@@ -10,3 +10,12 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given url does not match \
+                valid image extensions')
+        return url
