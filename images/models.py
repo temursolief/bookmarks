1f4871c729710e8d31c6d0dc38ba1b72a68137bc
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 06:59:20 +0500

    added Image model to images.models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, blank=True)
+    url = models.URLField(max_length=256)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+        related_name='images_created', on_delete=models.CASCADE)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+        related_name = 'images_liked', blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields = ['-created'])
+        ]
+        ordering = ['-created']
+
+    def save(self, *args, **kwargs):
+        """
+        ? overriding the native save method
+        ? to slugify the title of the image 
+        """
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
