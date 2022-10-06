fc7a6f55dbb1b746e7f5bef96db424e523540057
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 10:24:22 +0500

    added image_detail to urlpatterns

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import image_create
+from .views import image_create, image_detail
 
 
 app_name = 'images'
@@ -7,5 +7,7 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', image_create, name='create'),
+    path('detail/<int:id>/<slug:slug>/', 
+        image_detail, name='detail'),
 ]
 
