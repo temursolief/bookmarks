89cc0d0d478cf3b79bbe7abf43f2d4ad3d830fa3
Timur Soliev <temursoliev2110@gmail.com>
2022-10-06 10:26:15 +0500

    added get_absolute_url to Image Model

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -21,6 +22,10 @@ class Image(models.Model):
             models.Index(fields = ['-created'])
         ]
         ordering = ['-created']
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, 
+            self.slug])
 
     def save(self, *args, **kwargs):
         """
