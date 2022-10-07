07080de4e57e937a81aa5d438d9f9c9c371fb00f
Timur Soliev <temursoliev2110@gmail.com>
2022-10-07 14:04:56 +0500

    added contenttype model and fields to Action model

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Action(models.Model):
@@ -6,9 +8,14 @@ class Action(models.Model):
             related_name='actions', on_delete=models.CASCADE)
     verb = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType, blank=True,
+            null=True, related_name='target_obj', on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
+            models.Index(fields=['target_ct', 'target_id']),
         ]
         ordering = ['-created',]
