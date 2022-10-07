9117ee59f10f1d9e27fa51b8480c2b7531aa862b
Timur Soliev <temursoliev2110@gmail.com>
2022-10-07 13:30:44 +0500

    added user_follow url handler to urlpatterns

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from .views import (dashboard, register, edit, 
-        user_login, user_list, user_detail)
+        user_login, user_list, user_detail, user_follow)
 
 urlpatterns = [
     # # path('login/', user_login, name='login'),
@@ -37,5 +37,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('edit/', edit, name='edit'),
     path('users/', user_list, name='user_list'),
+    path("users/follow/", user_follow, name='user_follow'),
     path('users/<username>/', user_detail, name='user_detail'),
 ]
