40479c93b70e24dadf4b750af5b4b5ccd0f79a72
Timur Soliev <temursoliev2110@gmail.com>
2022-10-07 15:21:42 +0500

    updated dashboard with select_related db query

--- account/views.py
+++ account/views.py
@@ -62,7 +62,7 @@ def dashboard(request):
     following_ids = request.user.following.values_list('id', flat=True)
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')[:10]
     return render(request, 
         'account/dashboard.html',
         {'section': 'dashboard',
