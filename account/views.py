228b20d4b495e6fb3787e08a1dc4b4778a5928b6
Timur Soliev <temursoliev2110@gmail.com>
2022-10-07 15:23:44 +0500

    updated dashboard view with prefetch_related db query

--- account/views.py
+++ account/views.py
@@ -62,11 +62,14 @@ def dashboard(request):
     following_ids = request.user.following.values_list('id', flat=True)
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__profile')[:10]
+    actions = actions.select_related('user', 'user__profile')\
+        .prefetch_related('target')[:10]
     return render(request, 
         'account/dashboard.html',
         {'section': 'dashboard',
         "actions": actions})
+
+        
 
 def user_login(request):
     """
